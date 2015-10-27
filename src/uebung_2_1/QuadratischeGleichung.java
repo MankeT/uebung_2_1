bdd79405b18d5b03941033953392e46ec7179723
MankeT <tim.manke@nordakademie.de>
2015-10-27 11:14:57 +0100

    Aktualisierung

--- src/uebung_2_1/QuadratischeGleichung.java
+++ src/uebung_2_1/QuadratischeGleichung.java
@@ -5,15 +5,15 @@ public class QuadratischeGleichung {
 	public static void main(String[] args) {
 		double p = 8;
 		double q = 15;
-		
-		double unterderwurzel = ((p/2)*(p/2)-q);
-		double vorderwurzel = -1 * (p/2);
-		
+
+		double unterderwurzel = ((p / 2) * (p / 2) - q);
+		double vorderwurzel = -1 * (p / 2);
+
 		double loesung1 = vorderwurzel + Math.sqrt(unterderwurzel);
 		double loesung2 = vorderwurzel - Math.sqrt(unterderwurzel);
-		
-		System.out.println ("x1 =" + loesung1);
-		System.out.println ("x2 =" + loesung2);
+
+		System.out.println("x1 =" + loesung1);
+		System.out.println("x2 =" + loesung2);
 
 	}
 
